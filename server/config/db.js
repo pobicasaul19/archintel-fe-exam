4c7aef948c396c3d4298b64afc28490d885c1f6d
Pobi Casaul <phoebecasaul@gmail.com>
2024-11-26 13:08:10 +0800

    add logic to export the schema into json

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,25 @@ const mongodb = require('mongodb');
 const uri = process.env.URI;
 
 // Helper function to get a MongoDB collection dynamically
+// const getCollection = async (collectionName) => {
+//   try {
+//     const client = await mongodb.MongoClient.connect(uri);
+//     return client.db('DB').collection(collectionName);
+//   } catch (error) {
+//     throw new Error('Failed to connect to the database.');
+//   }
+// };
 const getCollection = async (collectionName) => {
+  const client = new mongodb.MongoClient(uri, { useUnifiedTopology: true });
   try {
-    const client = await mongodb.MongoClient.connect(uri);
+    await client.connect();
     return client.db('DB').collection(collectionName);
   } catch (error) {
+    console.error('Database connection failed:', error.message); // Log detailed error
     throw new Error('Failed to connect to the database.');
   }
 };
+
 
 // Predefined collection loaders for common collections
 const loadUserCollection = () => getCollection('user');
