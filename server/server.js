9613e1a2f7cda85c98f46b570f79d5b6b51d5e0a
Pobi Casaul <phoebecasaul@gmail.com>
2024-11-26 20:23:45 +0800

    update server

--- server/server.js
+++ server/server.js
@@ -32,4 +32,4 @@ app.use('/api/users/update', verifyToken, update)
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port);
